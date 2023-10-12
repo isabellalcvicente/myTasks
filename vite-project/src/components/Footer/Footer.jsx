9b86e38648544e9235d26d83edef8f7013adf4d9
isabellalcvicente <isabellavicente@ufpr.br>
2023-10-11 21:13:56 -0300

    feat: criação da pasta components com o componente Footer, e a pasta LandingPage

---
+++ vite-project/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <p> Desenvolvido por Isabella Vicente </p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ vite-project/src/page/LandingPage/LandingPage.jsx
@@ -0,0 +1,11 @@
+//aqui tu chama os outros componentes
+
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div>LandingPage</div>
+  )
+}
+
+export default LandingPage
