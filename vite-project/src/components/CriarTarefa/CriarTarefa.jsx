5e02d47b2ad8d07f8eb4d13183c2a0b52f3ae495
isabellalcvicente <isabellavicente@ufpr.br>
2023-10-12 08:15:33 -0300

    feat: criação do arquivo CriarTarefa com const se setagem

---
+++ vite-project/src/components/CriarTarefa/CriarTarefa.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+const CriarTarefa = () => {
+    const[criarTarefa,setCriarTarefa]=useState("")
+
+
+
+
+  return (
+    <div>CriarTarefa</div>
+  )
+}
+
+export default CriarTarefa
